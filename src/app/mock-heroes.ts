b4ccd340f48f8c33b0df18ee6cd4b5d4f204307f
Saransh Sharma <saranshs111@gmail.com>
2018-12-15 19:06:47 +0530

    Final Tour of heroes app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero} from './hero';
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Thanos' },
+    { id: 12, name: 'Hawkeye' },
+    { id: 13, name: 'Hulk' },
+    { id: 14, name: 'Thor' },
+    { id: 15, name: 'Iron man' },
+    { id: 16, name: 'Spiderman' },
+    { id: 17, name: 'Black Widow' },
+    { id: 18, name: 'Scarlett Witch' },
+    { id: 19, name: 'Vision' },
+    { id: 20, name: 'Black Panther' }
+  ];
